a2244b011172c1de4f2b98015452985a28f90edd
Martin <matiasgls@hotmail.com>
2024-01-08 19:02:52 -0300

    75%

--- Python/main.py
+++ Python/main.py
@@ -1,12 +1,23 @@
 import mysql.connector
+import csv
 
-conn = mysql.connector.connect(
-    user='root', password='arbolesroot123', host='mysql', port=3306, database='arboles')
+with open("/usr/src/app/dataset/caracteristicas.csv",'r+', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    
+    conn = mysql.connector.connect(
+           user='', password='arbolesroot123', host='mysql', port=3306, database='arboles')
+    
+    print('Connection successful')
+    
+    cursor = conn.cursor()
 
-cursor = conn.cursor()
-print("DB connected")
+    for row in reader:
+        cursor.execute('INSERT INTO Caracteristicas(tipo_folla) VALUES(%s)',row)
+    
+    conn.commit()
 
-cursor.execute('SHOW TABLES;')
-arboles = cursor.fetchall()
-conn.close()
-print(f'{arboles}')
+    cursor.close()
+
+    conn.close()
+ 
+
--- mysql/arboles.sql
+++ mysql/arboles.sql
@@ -1,4 +1,4 @@
-use arboles;
+use arboles
 
 CREATE TABLE if not exists infoArbol (
     id_arbol int not null AUTO_INCREMENT,
@@ -18,22 +18,26 @@ CREATE TABLE if not exists Coordenadas (
 );
 
 CREATE TABLE if not exists espacioVerde (
-    espacio_ve varchar(100)
-    ubicacion varchar(100)
+    espacio_ve varchar(100),
+    ubication varchar(200)
 );
 
 CREATE TABLE if not exists origenArbol (
-    nombre_fam varchar(100)
+    nombre_fam varchar(100),
     nombre_gen varchar(100)
 );
 
 CREATE TABLE if not exists infoEspecie (
-    id_especie int NOT NULL
-    nombre_comp varchar(100)
+    id_especie int NOT NULL,
+    nombre_comp varchar(100),
     nombre_cie varchar(100)
 
+);
+
 CREATE TABLE if not exists Caracteristicas (
-    tipo_folla varchar(100)
+    tipo_folla varchar(200)
+
+);
     
 
 
